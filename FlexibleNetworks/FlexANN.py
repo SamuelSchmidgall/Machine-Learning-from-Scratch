81bdf3603ba280b4446ea1e4e52e4f579cd59af9
AbstractMobius <abstractmobius@gmail.com>
2018-05-07 01:39:50 -0400

    shifted folders around, added biases to artificial neural network

---
+++ FlexibleNetworks/FlexANN.py
@@ -0,0 +1,20 @@
+from NeuralNetworks.ArtificialNeuralNetwork import ArtificialNeuralNetwork
+
+class FlexANN:
+    """ An artificial neural network that generates itself """
+    def __init__(self):
+        pass
+
+    def _generate_network(self):
+        pass
+
+    def train(self, inputs, expected_outputs):
+        pass
+
+
+# what we want is for every neural network to be saying is YES or NO
+
+
+
+
+
--- NeuralNetworks/ArtificialNeuralNetwork.py
+++ NeuralNetworks/ArtificialNeuralNetwork.py
@@ -2,14 +2,18 @@ import math
 import numpy as np
 
 class ArtificialNeuralNetwork:
-    def __init__(self, dimensions):
+    def __init__(self, dimensions, weights=None):
         """ Instantiate Neural Network """
         if not all(element > 0 for element in dimensions):
             raise Exception("Invalid input size")
         self._input_length  = dimensions[0]
         self._output_length = dimensions[-1]
         # generate weights where for each matrix the rows represent the weights connecting to a node in the next layer
-        self._weights = [np.random.uniform(-1, 1, (dimensions[itr+1], dimensions[itr])) for itr in range(len(dimensions)-1)]
+        if weights is None:
+            self._weights = [np.random.uniform(-1, 1, (dimensions[itr+1], dimensions[itr])) for itr in range(len(dimensions)-1)]
+        else:
+            self._weights = self._load_weights(weights)
+        self.biases = [np.random.uniform(-1, 1, (dimensions[itr+1])) for itr in range(len(dimensions)-1)]
 
     def _sigmoid(self, x):
         """ Sigmoid function """
@@ -19,14 +23,22 @@ class ArtificialNeuralNetwork:
         """ Derivative of sigmoid function """
         return sigmoid_value*(1.0 - sigmoid_value)
 
-    def _activate(self, weights, inp):
+    def _activate(self, weights, inp, weight_val):
         """ Activation function """
-        return self._sigmoid(np.matmul(weights,inp))
+        return self._sigmoid(np.add(np.matmul(weights,inp), self.biases[weight_val]))
 
     def forward_prop(self, inputs):
         """ Forward propagation """
         value, outputs = self._forward_prop(inputs)
         return value
+
+    def save(self, filename):
+        """ Save the weights of your neural network """
+        np.save(filename, self._weights)
+
+    def _load_weights(self, filename):
+        """ Private method: load weights into neural network """
+        return np.load(filename)
 
     def _forward_prop(self, inputs):
         """ Forward propagation with output values for backpropagation"""
@@ -36,14 +48,14 @@ class ArtificialNeuralNetwork:
             inputs = np.array(inputs)
         outputs = [inputs]
         for itr in range(len(self._weights)):
-            inputs = self._activate(self._weights[itr], inputs) # update values to propagate
+            inputs = self._activate(self._weights[itr], inputs, itr) # update values to propagate
             outputs.append(inputs) # keep track of outputs for backpropagation
         return inputs, np.array(outputs[:-1])
 
     def back_prop(self, inputs, exp_val, learning_rate=0.01):
         """ Backpropagation algorithm using matrix algebra """
         if len(exp_val) != self._output_length or type(exp_val) not in (np.ndarray, list):
-            raise Exception("Invalid expected value")
+            raise Exception("Invalid expected value -- check size or type")
         elif type(exp_val) is list:
             exp_val = np.array(exp_val)
         ret_val, output_values = self._forward_prop(inputs) # forward prop values
@@ -53,6 +65,7 @@ class ArtificialNeuralNetwork:
         error = np.add(targets, -1 * ret_val) # generate initial error
         gradients = np.multiply(self._sigmoid_derivative(ret_val), error) * learning_rate # get initial gradients
         weight_deltas = np.matmul(gradients, hidden.T) # generate initial weight deltas
+        self.biases[-1] = np.add(self.biases[-1], gradients)
         self._weights[-1] = np.add(self._weights[-1], weight_deltas) # update weights based on weight deltas
         for itr in reversed(range(len(self._weights))[1:]):
             # resize into vector format and update hidden layer weights
@@ -60,6 +73,25 @@ class ArtificialNeuralNetwork:
             weight_m = self._weights[itr].T # generate transpose of corresponding weight matrix
             error = np.matmul(weight_m, error) # calculate error
             gradients = np.multiply(self._sigmoid_derivative(hidden), error)*learning_rate # calculate gradient
+            self.biases[itr-1] = np.add(self.biases[itr-1], np.resize(gradients, (len(self.biases[itr-1]))))
             weight_deltas = np.matmul(gradients, inputs.T) # generate weight deltas
             self._weights[itr-1] = np.add(self._weights[itr-1], weight_deltas) # update weights
             hidden = np.resize(output_values[itr-1], (len(output_values[itr-1]), 1)) # update hidden layer outputs
+
+
+ann = ArtificialNeuralNetwork([1, 30, 40, 1])
+for i in range(10000):
+    ann.back_prop([10], [0])
+    ann.back_prop([9], [1])
+    ann.back_prop([8], [0])
+    ann.back_prop([7], [1])
+    ann.back_prop([6], [0])
+    ann.back_prop([5], [1])
+    ann.back_prop([4], [0])
+    ann.back_prop([3], [1])
+    ann.back_prop([2], [0])
+    ann.back_prop([1], [1])
+    ann.back_prop([0], [0])
+
+for i in range(11):
+    print(ann.forward_prop([i]))
