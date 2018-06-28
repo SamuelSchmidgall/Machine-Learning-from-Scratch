f7cc988e8e2bd085c4006b4a53507f4c8f6d8728
AbstractMobius <abstractmobius@gmail.com>
2018-06-28 13:05:37 -0400

    added simple linear regression model

--- Regression/SimpleLinear.py
+++ Regression/SimpleLinear.py
@@ -1,43 +1,50 @@
-a ="""
- 1  1  39  144
- 2  1  47  220
- 3  1  45  138
- 4  1  47  145
- 5  1  65  162
- 6  1  46  142
- 7  1  67  170
- 8  1  42  124
- 9  1  67  158
-10  1  56  154
-11  1  64  162
-12  1  56  150
-13  1  59  140
-14  1  34  110
-15  1  42  128
-16  1  48  130
-17  1  45  135
-18  1  17  114
-19  1  20  116
-20  1  19  124
-21  1  36  136
-22  1  50  142
-23  1  39  120
-24  1  21  120
-25  1  44  160
-26  1  53  158
-27  1  63  144
-28  1  29  130
-29  1  25  125
-30  1  69  175
-"""
-k = []
-for i in a.split("\n"):
-    if len(i) > 0:
-        b = i.split(" ")[4:]
-        b = [int(j) for j in b if j != '']
-        k.append(b)
 
-# y = wx + b
+class SimpleLinear:
+    def __init__(self, dataset):
+        """ Generate simple linear regression model """
+        try:
+            if self._is_valid_dataset(dataset):
+                self._dataset = dataset
+        except IndexError:
+            raise Exception("Incompatible Dataset: REQUIRED - list of 2 variable tuples")
+        self._cof = self._coefficients(self._dataset)
+
+    def predict(self, value):
+        """ Predict value based on computed regression """
+        return self._cof[0] + self._cof[1]*value
+
+    def _mean(self, values):
+        """ Mean of list of data values """
+        if len(values) > 0:
+            return sum(values)/len(values)
+
+    def _variance(self, values):
+        """ Variance of list of data values """
+        mean_v = self._mean(values)
+        return sum([(x - mean_v) ** 2 for x in values])
+
+    def _covariance(self, x, y):
+        """ Covariance of list of data values """
+        mean_x, mean_y, = self._mean(x), self._mean(y)
+        return sum((x[val] - mean_x) * (y[val] - mean_y) for val in range(len(x)))
+
+    def _coefficients(self, dataset):
+        """ Generate polynomial coefficents for list of data values """
+        x, y = [row[0] for row in dataset], [row[1] for row in dataset]
+        b1 = self._covariance(x, y) / self._variance(x)
+        x_mean, y_mean = self._mean(x), self._mean(y)
+        b0 = y_mean - b1 * x_mean
+        return [b0, b1]
+
+    def _is_valid_dataset(self, dataset):
+        """ Check if dataset contains values that can be computed """
+        truth = [len(dataset[i]) == 2 and \
+          (type(dataset[i][0]) == int and type(dataset[i][1]) == int) or \
+          (type(dataset[i][0]) == float and type(dataset[i][1])) == float
+          for i in range(len(dataset))]
+        return all(truth)
+
+
 
 
 
