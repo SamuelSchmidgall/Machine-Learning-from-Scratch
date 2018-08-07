3ce6c19946c003eefdcfde14550b6770d75b9885
Samuel Schmidgall <AbstractMobius@gmail.com>
2018-08-07 15:18:47 -0400

    changes to multiple and simple regression

--- SciGen/Regression/MultipleLinear.py
+++ SciGen/Regression/MultipleLinear.py
@@ -7,52 +7,78 @@ import numpy as np
 
 
 class MultipleLinear:
-    def __init__(self, dataset):
+    def __init__(self):
         """
-            Multiple Linear Regression Model
-            Value that needs to be predicted should be last value of each tuple
+        Multiple Linear Regression Model
         """
-        try:
-            self._tuple_size = len(dataset[0])
-            if all([dataset[i] == self._tuple_size for i in range(len(dataset))]):
-                self._dataset = dataset
-        except IndexError:
-            raise Exception("Invalid Dataser")
-        self._dataset_len = len(dataset)
-        self.inp, self.pred, self.bias = self._generate_init_values(dataset)
+        self._tuple_size = None
+        self._data_set_len = None
+        self.inp, self.pred, self.bias = None, None, None
+
+    def train(self, inputs, exp_val):
+        """
+        Train the given model based on inputs and expected value
+        :param inputs: input value which is expected to return corresponding exp_val
+        :param exp_val: expected value for a given input
+        """
+        if len(inputs) != len(exp_val) <= 0:
+            raise Exception('Invalid Data Set')
+        self._tuple_size = len(inputs[0])
+        self._data_set_len = len(inputs)
+        self.inp, self.pred, self.bias = self._generate_init_values(inputs, exp_val)
         self._gradient_descent()
 
     def predict(self, pred_vals):
-        """ Compute multiple linear regression prediction """
+        """
+        Compute multiple linear regression prediction
+        :param pred_vals: ndarray -> values to predict
+        :return: float -> predicted value
+        """
+        if self.inp is None or self.pred is None or self.bias is None:
+            raise Exception('Model must be trained before prediction can occur')
         if type(pred_vals) == list:
             pred_vals = np.array(pred_vals)
-        pred_vals = np.insert(pred_vals,0,1)
+        pred_vals = np.insert(pred_vals, 0, 1)
         return pred_vals.dot(self.bias)
 
     def _cost_function(self):
-        """ Cost function """
-        return np.sum((self.inp.dot(self.bias) - self.pred) ** 2) / (2 * self._dataset_len)
+        """
+        Cost function
+        """
+        return np.sum((self.inp.dot(self.bias) - self.pred) ** 2) / (2 * self._data_set_len)
 
-    def _generate_init_values(self, dataset):
-        """ Generate initial values """
-        return np.array([np.ones(len(dataset))]+ \
-          [np.array([k[i] for k in dataset]) for i in range(self._tuple_size-1)]).T, \
-          np.array([i[-1] for i in dataset]), np.zeros(self._tuple_size)
+    def _generate_init_values(self, x, y):
+        """
+        Generate initial values
+        :param x: list(tuple(float)) -> list of x values
+        :param y: list(tuple(float)) -> list of y values
+        :return: ndarray -> initialized variables
+        """
+        return np.array([np.ones(len(x))] + [np.array([k[i] for k in x]) for i in range(self._tuple_size)]).T,\
+               np.array([i[-1] for i in y]), np.zeros(self._tuple_size+1)
 
     def _gradient_descent(self, learning_rate=0.0001, iterations=100000):
-        """ Perform gradient decent to train model """
+        """
+        Perform gradient decent to train model
+        :param learning_rate: float -> learning rate
+        :param iterations: int -> training iterations
+        """
         r_itr = range(iterations)
-        for itr in r_itr:
+        for _ in r_itr:
             loss = self.inp.dot(self.bias) - self.pred
-            gradient = self.inp.T.dot(loss) / self._dataset_len
+            gradient = self.inp.T.dot(loss) / self._data_set_len
             self.bias = self.bias - learning_rate * gradient
 
-    def model_efficency(self, Y_pred):
-        """ Test the efficency of trained model """
-        if type(Y_pred) == list:
-            Y_pred = np.array(Y_pred)
-        mean_s_err = np.sqrt(sum((self.pred - Y_pred) ** 2) / len(self.pred))
+    def model_efficiency(self, y_pred):
+        """
+        Test the efficency of trained model
+        :param y_pred: ndarray -> y prediction vector
+        :return: tuple(float, float) -> efficiency of given model
+        """
+        if type(y_pred) == list:
+            y_pred = np.array(y_pred)
+        mean_s_err = np.sqrt(sum((self.pred - y_pred) ** 2) / len(self.pred))
         y_mean = np.mean(self.pred)
-        r2_score = 1 - (sum((self.pred - y_mean) ** 2) / sum((self.pred - Y_pred) ** 2))
+        r2_score = 1 - (sum((self.pred - y_mean) ** 2) / sum((self.pred - y_pred) ** 2))
         return mean_s_err, r2_score
 
--- SciGen/Regression/SimpleLinear.py
+++ SciGen/Regression/SimpleLinear.py
@@ -5,53 +5,66 @@ __email__ = "sschmidg@masonlive.gmu.edu"
 
 
 class SimpleLinear:
-    def __init__(self, dataset):
-        """ Generate simple linear regression model """
-        try:
-            if self._is_valid_dataset(dataset):
-                self._dataset = dataset
-        except IndexError:
-            raise Exception("Incompatible Dataset: REQUIRED - list of 2 variable tuples")
-        self._cof = self._coefficients(self._dataset)
+    def __init__(self):
+        """
+        Generate simple linear regression model
+        """
+        self._cof = None
+        self._data_set = None
+
+    def train(self, inputs, exp_val):
+        """
+        Generate linear regression coefficients based on given data set
+        :param inputs: list(float) -> list of input x values
+        :param exp_val: list(float) -> list of output y values that are expected
+        :return:
+        """
+        self._cof = self._coefficients(inputs, exp_val)
 
     def predict(self, value):
-        """ Predict value based on computed regression """
+        """
+        Predict value based on computed regression
+        :param value: float -> value to predict
+        :return: predicted value from regression model
+        """
         return self._cof[0] + self._cof[1]*value
 
-    def _mean(self, values):
-        """ Mean of list of data values """
+    @staticmethod
+    def _mean(values):
+        """
+        Mean of list of data values
+        :param values: list(float) -> values to generate mean from
+        :return: float -> mean of values
+        """
         if len(values) > 0:
             return sum(values)/len(values)
 
     def _variance(self, values):
-        """ Variance of list of data values """
+        """
+        Variance of list of data values
+        :param values: list(float) -> list of values to calculate variance from
+        :return: float -> variance of values
+        """
         mean_v = self._mean(values)
         return sum([(x - mean_v) ** 2 for x in values])
 
     def _covariance(self, x, y):
-        """ Covariance of list of data values """
+        """
+        Covariance of list of data values
+        :param x: list(float) -> list of x variables
+        :param y: list(float) -> list of y variables
+        :return: float -> covariance of lists
+        """
         mean_x, mean_y, = self._mean(x), self._mean(y)
         return sum((x[val] - mean_x) * (y[val] - mean_y) for val in range(len(x)))
 
-    def _coefficients(self, dataset):
-        """ Generate polynomial coefficents for list of data values """
-        x, y = [row[0] for row in dataset], [row[1] for row in dataset]
+    def _coefficients(self, x, y):
+        """
+        Generate polynomial coefficients for list of data values
+        :param data_set: list(tuple(x, y)) -> variables to train model on
+        :return: list(float) -> list of coefficients
+        """
         b1 = self._covariance(x, y) / self._variance(x)
         x_mean, y_mean = self._mean(x), self._mean(y)
         b0 = y_mean - b1 * x_mean
         return [b0, b1]
-
-    def _is_valid_dataset(self, dataset):
-        """ Check if dataset contains values that can be computed """
-        truth = [len(dataset[i]) == 2 and \
-          (type(dataset[i][0]) == int and type(dataset[i][1]) == int) or \
-          (type(dataset[i][0]) == float and type(dataset[i][1])) == float
-          for i in range(len(dataset))]
-        return all(truth)
-
-
-
-
-
-
-
