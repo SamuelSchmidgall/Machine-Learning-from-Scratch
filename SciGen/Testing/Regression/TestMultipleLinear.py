dec466d49c5f54159d3ef648f9bde86df987b201
Samuel Schmidgall <AbstractMobius@gmail.com>
2018-08-07 10:31:33 -0400

    removed testing portion

--- SciGen/Testing/Regression/TestMultipleLinear.py
+++
@@ -1,6 +0,0 @@
-import math
-from SciGen.Regression.MultipleLinear import MultipleLinear
-
-data = [[48,68,63],[62,81,72],[79,80,78],[76,83,79],[59,64,62]]
-ml = MultipleLinear(data)
-print(math.floor(ml.predict([48, 68])) == 61)
